8554b5c027f3a10fed239a7ad04cf8aa9fcd7861
kstapes <46645798+kstapes@users.noreply.github.com>
2020-05-18 17:10:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            OddNumbers(numbers);
+
+        }
+
+        static void OddNumbers(int[] numbers)
+        {
+            Console.WriteLine("Odd Numbers: ");
+
+            IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number;
+            Console.WriteLine(oddNumbers);
+
+            foreach(int i in oddNumbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
